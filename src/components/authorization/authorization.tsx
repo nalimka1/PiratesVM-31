db952c5109c7c460ceb5c17774cc76ef30a9caec
Andrey <andrey949495@yandex.ru>
2020-10-14 18:05:19 +0400

    feat: auth using sockets

--- src/components/authorization/authorization.tsx
+++ src/components/authorization/authorization.tsx
@@ -1,14 +1,14 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import AuthContext from '../../contexts/auth.context';
 import styled from 'styled-components';
 import Button from '../button/button';
 import Input from '../input/input';
 import Banner from '../banner/banner';
-import useHttp from '../../hooks/http.hook';
 import md5 from 'md5';
 import authBackground from '../../assets/auth.png';
-import { METHODS } from '../../constants/methods.contants';
 import { passwordReg } from '../../constants/authorization.constants';
+import socket from '../../helpers/socket';
+import { SOCKET_EVENTS } from '../../constants/socket.constants';
 
 const Container = styled.div`
   display: flex;
@@ -32,10 +32,13 @@ const ControlButtons = styled.div`
 `;
 
 const Authorization = () => {
-  const { request } = useHttp();
   const { login } = useContext(AuthContext);
   const [form, setForm] = useState({ login: '', password: '' });
-  const [error, setError] = useState('');
+
+  useEffect(() => {
+    socket.on(SOCKET_EVENTS.USER_LOGIN, (data) => console.log(data))
+    socket.on(SOCKET_EVENTS.USER_SIGNUP, (data) => console.log(data))
+  }, []);
 
   const isValidPassword = () => {
     return passwordReg.test(
@@ -51,45 +54,20 @@ const Authorization = () => {
   };
 
   const handleLogin = async (event) => {
-    try {
-      event.preventDefault();
-      setError('');
-      if (isValidPassword()) {
-        const random = Math.random();
-        const hash = md5(md5(form.password + form.login) + random);
-        const data = await request(
-          'http://localhost:8080/api/auth',
-          METHODS.POST,
-          {
-            login: form.login,
-            hash,
-            random,
-          },
-        );
-        login(data.token);
-      }
-    } catch (err) {
-      setError(err.message);
+    event.preventDefault();
+    if (isValidPassword()) {
+      const random = Math.random();
+      const hash = md5(md5(form.password + form.login) + random);
+      socket.emit(SOCKET_EVENTS.USER_LOGIN, { login: form.login, hash, random });
     }
   };
 
   const handleSignup = async (event) => {
-    try {
-      event.preventDefault();
-      if (form.login && isValidPassword()) {
-        const hash = md5(form.password + form.login);
-        const data = await request(
-          'http://localhost:8080/api/signup',
-          METHODS.POST,
-          {
-            login: form.login,
-            hash,
-          },
-        );
-        login(data.token);
-      }
-    } catch (err) {
-      setError(err.message);
+    event.preventDefault();
+    const { login } = form;
+    if (login && isValidPassword()) {
+      const hash = md5(form.password + login);
+      socket.emit(SOCKET_EVENTS.USER_SIGNUP, { login, hash })
     }
   };
 
---
+++ src/constants/socket.constants.ts
@@ -0,0 +1,7 @@
+export const SOCKET_URL = 'localhost:9000';
+
+export const SOCKET_EVENTS = {
+  USER_LOGIN: 'userLogin',
+  USER_SIGNUP: 'userSignup',
+  SEND_MESSAGE: 'sendMessage',
+};
