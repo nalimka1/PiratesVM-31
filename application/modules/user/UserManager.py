b801f2141a5613358b3ee9a25b13ce68786bae4c
Andrey <andrey949495@yandex.ru>
2020-10-14 18:06:09 +0400

    feat: authorization and registration using sockets

--- application/modules/user/UserManager.py
+++ application/modules/user/UserManager.py
@@ -14,6 +14,8 @@ class UserManager(BaseManager):
         # регистрируем события
         self.mediator.subscribe(self.EVENTS['INSERT_USER'], self.__insertUser)
         self.mediator.subscribe(self.EVENTS['UPDATE_TOKEN_BY_LOGIN'], self.__updateTokenByLogin)
+        self.sio.on('userLogin', self.auth)
+        self.sio.on('userSignup', self.registration)
 
     def __generateToken(self, login):
         if login:
@@ -21,47 +23,55 @@ class UserManager(BaseManager):
             return self.__generateHash(login, str(randomInt))
 
     def __generateHash(self, str1, str2=""):
+        print(str1, str2)
         if type(str1) == str and type(str2) == str:
             return hashlib.md5((str1 + str2).encode("utf-8")).hexdigest()
         return None
 
     def __getUserByToken(self, data=None):
         if data:
-            return self.__db.getUserByToken(data['token'])
+            return self.db.getUserByToken(data['token'])
         return None
 
     def __getUserByLogin(self, data):
         if data:
-            return self.__db.getUserByLogin(data['login'])
+            return self.db.getUserByLogin(data['login'])
         return None
 
     def __getHashByLogin(self, data):
+        print(data)
         if data:
-            return self.__db.getHashByLogin(data['login'])
+            return self.db.getHashByLogin(data['login'])
         return None
 
     def __insertUser(self, data):
         if data:
-            return self.__db.insertUser(data['name'], data['login'], data['password'], data['token'])
+            return self.db.insertUser(data['name'], data['login'], data['password'], data['token'])
         return None
 
     def __updateTokenByLogin(self, data):
         if data:
-            return self.__db.updateTokenByLogin(data['login'], data['token'])
+            return self.db.updateTokenByLogin(data['login'], data['token'])
         return None
 
-    def registration(self, name, login, password):
+    def registration(self, sio, data):
+        print(data)
+        name = data['login']
+        login = data['login']
+        password = data['hash']
         user = self.mediator.get(self.TRIGGERS['GET_USER_BY_LOGIN'], dict(login=login))
         if user or not name or not login or not password:
             return None
         else:
-            password = self.__generateHash(password)
             token = self.__generateToken(login)
             self.mediator.call(self.EVENTS['INSERT_USER'], dict(name=name, login=login, password=password, token=token))
             return True
 
-    def auth(self, login, hash, rnd):
-        print(login, hash, rnd)
+    async def auth(self, sio, data):
+        login = data['login']
+        hash = data['hash']
+        rnd = data['random']
+        print(data)
         if login and hash and rnd:
             hashDB = self.mediator.get(self.TRIGGERS['GET_HASH_BY_LOGIN'], dict(login=login))
             if self.__generateHash(hashDB, str(rnd)) == hash:
@@ -69,6 +79,7 @@ class UserManager(BaseManager):
                 self.mediator.call(self.EVENTS['UPDATE_TOKEN_BY_LOGIN'], dict(login=login, token=token))
                 # добавляем пользователя в список пользователей онлайн
                 self.mediator.call(self.EVENTS['ADD_USER_ONLINE'], dict(token=token))
+                await self.sio.emit('userLogin', True)
                 return True
         return False
 
