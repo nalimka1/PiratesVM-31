4626aa271701dce2b3c6d8feda62e663304da989
Andrey <andrey949495@yandex.ru>
2020-10-15 23:29:24 +0400

    refactor: remove unneeded files and the parameter ERRORS in UserManager

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ sio.attach(app)
 Router(app, web, mediator)
 Logic(mediator)
 
-UserManager(db, mediator, sio, SETTINGS['MESSAGES'], SETTINGS['ERRORS'])
+UserManager(db, mediator, sio, SETTINGS['MESSAGES'])
 ChatManager(db, mediator, sio, SETTINGS['MESSAGES'], SETTINGS['CHAT'])
 
 web.run_app(app, port=9000)
