ec00393cd5fadae10d57474be22e747327a72d2e
Andrey <andrey949495@yandex.ru>
2020-10-18 14:40:21 +0400

    refactor: move router, theme to index

---
+++ src/constants/theme-default.ts
@@ -0,0 +1,15 @@
+import { DefaultTheme } from 'styled-components';
+
+const theme: DefaultTheme = {
+  fg: '#ffe190',
+  fgError: '#d4eee8',
+  bg: '#237773',
+  bgActive: '#6fb38e',
+  scrollbarBorderRadius: '8px',
+  scrollbarWidth: '10px',
+  scrollbarBg: '#6fb38e',
+  scrollbarThumbWidth: '8px',
+  scrollbarThumbBg: '#1d3e3c',
+};
+
+export default theme;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import * as serviceWorker from './serviceWorker';
 import App from './components/app';
 import store from './redux/store';
 import './index.css';
+import { ThemeProvider } from 'styled-components';
+import theme from './constants/theme-default';
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <App/>
+        </ThemeProvider>
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
