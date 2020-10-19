c1d48560f32b208636fab3da8912b68138276ce0
AndreyBodulin <55592933+AndreyBodulin@users.noreply.github.com>
2020-10-19 20:08:37 +0400

    Добавил функцию kickFromTeam
    1. С клиента будет прилетать токен человека который выкидывает игрока?
    2. Можно ли выкинуть создателя комнаты

--- application/modules/lobby/Player.py
+++ application/modules/lobby/Player.py
@@ -1,5 +1,5 @@
 class Player:
-    def __init__(self, token, sid, readyToStart):
+    def __init__(self, token, sid, readyToStart=False):
         self.token = token
         self.sid = sid
         self.readyToStart = readyToStart
